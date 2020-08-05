073d8de33aa9bc241e9409206849ea448a5007a9
Mladen Trampic <mtrampic@hotmail.com>
2020-08-05 22:30:33 +0200

    build

--- setup.sh
+++ setup.sh
@@ -9,10 +9,14 @@ sudo yum upgrade -y
 sudo yum -y install \
     httpd
 
+#copy / override httpd.conf
+sudo cp /tmp/httpd/httpd.conf /etc/httpd/conf/httpd.conf
+
+#copy static content
+sudo cp -r /tmp/html/ /var/www/html/
+
 #enable HTTPD by default
 sudo systemctl enable httpd --now
 
 
 #configure virtualhost 
-
-#copy static content
